0b9ffd5e66e0162b4653681e77b2263e54d61726
repantoja <repantoja2002@gmail.com>
2018-09-11 21:09:45 +0100

    updates

--- app/components/chat-app/component.js
+++ app/components/chat-app/component.js
@@ -1,6 +1,19 @@
 import Component from '@ember/component';
-
+import {  inject as service } from '@ember/service';
+import { computed } from '@ember/object';
+import { getOwner } from '@ember/application';
 export default Component.extend({
+
+  createRoom: computed(()=>{
+      return getOwner(this).lookup('service:chat-service');
+  }),
+
+  actions: {
     
-    
+    add() {
+        let room = this.get('room')
+        //this.createRoom.add()
+        console.log(this.get('room'))
+      }
+  }
 });
